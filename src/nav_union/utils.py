85929e6bc843055929ad9af43515315e030bd18a
Espen Holtebu <espen.holtebu@nav.no>
2026-09-02 11:58:28 +0200

    Remor deault env function

--- src/nav_union/utils.py
+++ src/nav_union/utils.py
@@ -15,29 +15,3 @@ def default_image(python_version: str = "3.14") -> flyte.Image:
         "UV_KEYRING_PROVIDER": "subprocess", 
         "UV_DEFAULT_INDEX": f"https://oauth2accesstoken@{PYPI_PROXY_INDEX_URL}"
     })
-
-
-
-def create_default_task_env(
-        name: str,
-        python_version: str = "3.14",
-        pyproject_toml_path: str = "pyproject.toml"
-        pip) -> flyte.TaskEnvironment:
-    return flyte.TaskEnvironment(
-            name=name,
-            image=flyte.Image.from_base(
-              image_uri=f"{IMAGE_REGISTRY_BASE}/{IMAGE_NAME}:{python_version}-base"
-            )
-            .clone(
-                registry=IMAGE_REGISTRY_BASE,
-                name=IMAGE_NAME,
-                extendable=True,
-            )
-            .with_env_vars({
-              "UV_KEYRING_PROVIDER": "subprocess", 
-              "UV_DEFAULT_INDEX": f"https://oauth2accesstoken@{PYPI_PROXY_INDEX_URL}"
-            })
-            .with_env_vars({
-              "PYPROJECT_TOML_PATH": pyproject_toml_path
-            })
-    )
