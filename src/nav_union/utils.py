4f7c39af1caaece4e350e2bbd740d0e538c5b7c1
Erik Vattekar <erik.vattekar@nav.no>
2026-09-03 12:09:34 +0200

    feat: add default quarto image factory function

--- src/nav_union/utils.py
+++ src/nav_union/utils.py
@@ -18,5 +18,19 @@ def default_image(python_version: str = "3.14") -> flyte.Image:
     })
 
 
+def default_quarto_image(python_version: str = "3.14") -> flyte.Image:
+    return flyte.Image.from_base(
+        image_uri=f"{IMAGE_REGISTRY_BASE}/{IMAGE_NAME}:{python_version}-quarto-base"
+    ).clone(
+        registry=IMAGE_REGISTRY_BASE,
+        name=IMAGE_NAME,
+        extendable=True,
+        python_version=(int(python_version.split(".")[0]), int(python_version.split(".")[1]))
+    ).with_env_vars({
+        "UV_KEYRING_PROVIDER": "subprocess", 
+        "UV_DEFAULT_INDEX": f"https://oauth2accesstoken@{PYPI_PROXY_INDEX_URL}"
+    })
+
+
 if __name__ == "__main__":
     print(default_image())
